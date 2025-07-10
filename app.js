c4a85210ac8229974465af1138108a7986f30e8e
Gurucharan <72401652+Gurucharan-V@users.noreply.github.com>
2025-07-09 20:00:35 -0400

    day 3

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-const heading = React.createElement("h1", {}, "Hello from React");
-root.render(heading);
+
+
+
+const normaljs =  function() { return <h2>this JSX coming form regular js</h2>};
+
+const title = (
+    <h6>
+        this is a react functional form component using JSX
+    </h6>
+);
+    
+const Heading = () => <h3>this is a react functional component</h3>;
+
+let x = 10;
+const y = 20;
+
+const Layout = () => 
+     (
+
+        
+
+        <div>
+            <h1>
+                this is a react functional component using arrow function
+            </h1>
+            {<h1>{x + y}</h1>}
+            {<h2>{x * y}</h2>}
+            {normaljs()}
+            {title}
+            <Heading />
+        </div>
+    );
+
+root.render(<Layout />);
