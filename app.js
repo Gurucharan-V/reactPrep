ead5da0081b84e79efc39599e7bd0d6eda493228
Gurucharan <72401652+Gurucharan-V@users.noreply.github.com>
2025-10-19 11:20:46 -0400

    hi

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", null, "Hello, React!");
-
-const headingbody = React.createElement("div", { id: "headingbody" }, [
-  React.createElement(
-    "h1",
-    { id: "heading" },
-    "This is a heading inside the heading body."
-  ),
-  React.createElement(
-    "p",
-    { style: { color: "blue" } },
-    "This is a paragraph inside the heading body."
-  ),
-]);
+const block = <div className="firstContainer">
+  <h1>This is JSX</h1>
+  <p>This is a paragraph</p>
+</div>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(React.createElement(React.Fragment, null, heading, headingbody));
+root.render(block);
