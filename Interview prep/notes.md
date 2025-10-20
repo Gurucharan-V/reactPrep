0d0a30ee809ce12a2f9f59013e9069eea140d4cc
Gurucharan <72401652+Gurucharan-V@users.noreply.github.com>
2025-10-19 20:29:44 -0400

    Episode 3

---
+++ Interview prep/notes.md
@@ -0,0 +1,12 @@
+**React Components**
+1) *Class Base Components* - OLD way of writing code.
+2) *Functional Components* - NEW way of writing code.
+
+
+
+**Component Composition**
+1) Component inside component.
+
+**Cross Site Scripting**
+1) where I can run js through Browser.
+
--- app.js
+++ app.js
@@ -6,6 +6,61 @@ const block = <div className="firstContainer">
   <p>This is a paragraph</p>
 </div>;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const Address = () => {
+  return (
+    <div className="firstContainer">
+      <h4>Address</h4>
+      <p>123 React Lane, 53211</p>
+    </div>
+  );
+};
 
-root.render(block);
+
+let brr = [11, 12, 13];
+
+let g = brr.filter((i) => i%2 ===1);
+
+
+
+function Dateofbirth() {
+  const lines = Array.from({ length: 3 }).map((_, i) => i + 12);
+  return (<div> {lines}</div>);
+}
+
+let arr = [11, 12, 13];
+
+const Title = () => {
+  return <p>This is title for every section</p>;
+}
+
+
+
+
+
+const Gc = () => {
+  return (
+    <div className="headerContainer">
+      {/* <h1>Gourav Choudhary</h1>
+      {g}
+      <h1>Software Developer</h1>
+      <p>here is the profile</p>
+      <Address />
+      <Dateofbirth />
+      {block}
+      <div>{"\n"}</div>
+      {arr} */}
+
+
+      <div>
+        {Title()}
+        <Title />
+        <Title></Title>
+      </div>
+    </div>
+
+   
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Gc />);
